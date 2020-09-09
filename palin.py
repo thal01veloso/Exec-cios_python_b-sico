e1e5e024dbe5157657fc3009d0d6f4fbe3abc559
Thales veloso <thal01veloso@gmail.com>
2020-09-09 12:54:30 -0300

    Exercicio verificador de palindromo

---
+++ palin.py
@@ -0,0 +1,22 @@
+o = "1"
+b = []
+s = ''
+while o == "1":
+	a = input("Digite uma frase para analisar:\n ").lower()
+	tm = len(a)
+	for i in range(tm):
+		b.append(a[tm-1-i])
+	b = s.join(b)
+	print(b)
+	if b == a:
+		print('A frase é um palindromo: ')
+	else:
+		print('A Frase não é um palindromo')
+	o = input(""" Escolha uma opção abaixo:
+[1] Continuar
+[2] Sair 
+.
+.
+.
+""")
+
